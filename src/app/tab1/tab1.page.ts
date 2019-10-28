64a237435f3c11257725a3052ea2ef5964d72d01
Miriam <ente1993@yahoo.de>
2019-10-28 11:33:51 +0100

    remove debug rests

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -17,44 +17,13 @@ export class Tab1Page {
     exchangeReceived: ''
   }
 
-  listItems = [
-    "1. Aylin Roberts",
-    "2. Autumn Kuhic",
-    "3. Tiffany Windler",
-    "4. Sheila Bauch",
-    "5. Diana Gerhold",
-    "6. Arielle Kuhn"
-  ];
-
-/*   qsos = [
-    {
-      time: "12:23",
-      call: "DM7MD",
-      rstGiven: "59",
-      rstReceived: "59",
-      band: "2m"
-    },
-    {
-      time: "12:28",
-      call: "DF4NEN",
-      rstGiven: "59",
-      rstReceived: "59",
-      band: "2m"
-    },
-  ]; */
-
   qsos = [];
-
-
-  
 
   updateMyValue() {
     var now = new Date();
     var newQso = Object.assign({} , this.form); // copy content of object, don't link object itself!
     newQso.time = now.getHours() + ':' + now.getMinutes();
-    console.log(this.form.call);
     this.qsos.unshift(newQso);
-    //console.log(this.qsos);
   }
 
 }
