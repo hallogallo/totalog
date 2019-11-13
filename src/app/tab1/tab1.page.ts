3ee6624d61111df9b625b6dc7b5b07a2c12764fd
Miriam Hüpper (DM1RI) <dm1ri@dk0tu.de>
2019-11-13 12:25:32 +0100

    first draft of storage (does not work on android yet)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+
+  qsos: Array<any>;
+
+  constructor(private storage: Storage) {
+
+    let that = this;
+    
+    this.storage.get('qsos').then(function (value) {
+      that.qsos = value;
+    });
+
+  }
+
 
   form = {
     band: '',
@@ -17,13 +32,14 @@ export class Tab1Page {
     exchangeReceived: ''
   }
 
-  qsos = [];
-
   updateMyValue() {
-    var now = new Date();
-    var newQso = Object.assign({} , this.form); // copy content of object, don't link object itself!
-    newQso.time = now.getHours().toString().padStart(2 , '0') + ':' + now.getMinutes().toString().padStart(2 , '0') ;
+    let now = new Date();
+    let newQso = Object.assign({}, this.form); // copy content of object, don't link object itself!
+    newQso.time = now.getHours().toString().padStart(2, '0') + ':' + now.getMinutes().toString().padStart(2, '0');
+
     this.qsos.unshift(newQso);
+    this.storage.set('qsos', this.qsos)
+
   }
 
 }
