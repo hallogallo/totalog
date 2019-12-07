c994c664460c354a2f1d2ad2d814652db5eaf131
Miriam Hüpper (DM1RI) <dm1ri@dk0tu.de>
2019-12-07 22:48:47 +0100

    add settings page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' }
 ];
 @NgModule({
   imports: [
