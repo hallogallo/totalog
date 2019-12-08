5ea5c6aefcd688e11e46a1f5ae3fa2c9b15f13eb
Miriam Hüpper (DM1RI) <dm1ri@dk0tu.de>
2019-12-08 20:07:35 +0100

    code generator went crazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' }
+  }
 ];
 @NgModule({
   imports: [
