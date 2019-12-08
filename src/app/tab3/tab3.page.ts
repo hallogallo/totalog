49aa76bbf93984b57b00c61ba62ed5cf10a12372
Miriam Hüpper (DM1RI) <dm1ri@dk0tu.de>
2019-12-08 20:49:18 +0100

    add simple dark mode (not saved to storage yet)

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab3Page implements OnInit {
 
-  constructor() { }
+  darktoggle: any;
+  darkmode: boolean;
+
+  constructor() {
+
+    //this.darktoggle = document.querySelector('#themeToggle');
+    //document.body.classList.add('dark');
+    this.darkmode = false;
+
+  }
 
   ngOnInit() {
+  }
+
+  toggleDarkMode() {
+
+    this.darkmode = !this.darkmode;
+    if(this.darkmode == true) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+
+    console.log(this.darkmode);
+
   }
 
 }
