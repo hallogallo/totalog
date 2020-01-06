97cfc458284bcd82bafc07d0f65ea8df9d197451
Miriam Hüpper (DM1RI) <dm1ri@dk0tu.de>
2020-01-07 00:20:25 +0100

    save qso lists (references #1)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
+import { Tab1Page } from './../tab1/tab1.page';
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,95 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  qsoStorage: Storage;
+  qsoHistory: Array<any>;
+
+  constructor(private storage: Storage, private alertControl: AlertController) {
+
+    this.qsoStorage = storage;
+
+    this.storage.get('qsoHistory').then((value) => {
+
+      if ((value != null) && (value !== undefined)) {
+
+        this.qsoHistory = value;
+
+      } else {
+
+        this.qsoHistory = [];
+      }
+
+    }).catch((error) => {
+      console.log(error);
+      this.qsoHistory = [];
+    });
+
+  }
+
+  template = {
+    name:'',
+    timeSaved: '',
+    qsoList: [],
+    operator: '',
+  };
+
+  async saveQsoDialog() {
+    const alert = await this.alertControl.create({
+      header: 'Save QSOs',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'name'
+        },],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+          {
+            text: 'Save & keep recent QSOs',
+            handler: (alertData) => {
+              this.saveQsos(false, alertData.name);
+            }
+          },
+          {
+            text: 'Save & delete recent QSOs',
+            handler: (alertData) => {
+              this.saveQsos(true, alertData.name);
+            }
+          }
+        ]});
+
+        await alert.present();
+  }
+
+  async saveQsos(deleteRecentQsos: boolean, name: string) {
+
+    try {
+      const recentQsos = await this.storage.get('qsos');
+
+      if ((recentQsos == null) || (recentQsos == undefined)) {
+        return;
+      }
+
+      this.template.qsoList = recentQsos;
+      this.template.name = name;
+      const now = new Date();
+      this.template.timeSaved = now.getFullYear().toString() + '-' + (now.getMonth()+1).toString().padStart(2, '0') + '-' + now.getDate().toString().padStart(2, '0');
+
+      this.qsoHistory.unshift(this.template);
+      this.qsoStorage.set('qsoHistory', this.qsoHistory);
+      console.log(this.qsoHistory);
+
+    }
+    catch (error) { 
+      console.log(error);
+    }
+
+
+
+
+  }
 
 }
