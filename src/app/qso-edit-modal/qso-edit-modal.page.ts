6c659b2f7d21d0dadce7ef88a741c3fce6e7d7e7
Miriam Hüpper (DM1RI) <dm1ri@dk0tu.de>
2020-02-16 17:54:27 +0100

    edit QSO in history (fixes #5)

--- src/app/qso-edit-modal/qso-edit-modal.page.ts
+++ src/app/qso-edit-modal/qso-edit-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular'; 
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-qso-edit-modal',
@@ -11,7 +12,7 @@ export class QsoEditModalPage implements OnInit {
   qsoEditList: any;
   data: any;
 
-  constructor(public modalCtrl: ModalController, navParams: NavParams) { 
+  constructor(public modalCtrl: ModalController, navParams: NavParams, private alertControl: AlertController) { 
     this.qsoEditList = navParams.data.qsoList;
     this.data = navParams.data;
     console.log(navParams);
@@ -26,6 +27,68 @@ export class QsoEditModalPage implements OnInit {
 
   deleteQso(index: number) {
     this.qsoEditList.splice(index, 1);
+  }
+
+  async showEditDialog(qsoNumber: number) {
+    const alert = await this.alertControl.create({
+      header: 'Edit QSO',
+      inputs: [
+        {
+          name: 'call',
+          type: 'text',
+          value: this.qsoEditList[qsoNumber].call,
+          placeholder: 'Call'
+        },
+        {
+          name: 'band',
+          type: 'text',
+          id: 'name2-id',
+          value: this.qsoEditList[qsoNumber].band,
+          placeholder: 'Band'
+        },
+        {
+          name: 'rstGiven',
+          type: 'text',
+          value: this.qsoEditList[qsoNumber].rstGiven,
+          placeholder: 'RST TX'
+        },
+        {
+          name: 'rstReceived',
+          type: 'text',
+          value: this.qsoEditList[qsoNumber].rstReceived,
+          placeholder: 'RST RX'
+        },
+        {
+          name: 'exchangeGiven',
+          type: 'text',
+          value: this.qsoEditList[qsoNumber].exchangeGiven,
+          placeholder: 'Ex TX'
+        },
+        {
+          name: 'exchangeReceived',
+          type: 'text',
+          value: this.qsoEditList[qsoNumber].exchangeReceived,
+          placeholder: 'Ex RX'
+        }],
+
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        }, {
+          text: 'OK',
+          handler: (alertData) => {
+            this.qsoEditList[qsoNumber].call = alertData.call;
+            this.qsoEditList[qsoNumber].band = alertData.band;
+            this.qsoEditList[qsoNumber].rstGiven = alertData.rstGiven;
+            this.qsoEditList[qsoNumber].rstReceived = alertData.rstReceived;
+            this.qsoEditList[qsoNumber].exchangeGiven = alertData.exchangeGiven;
+            this.qsoEditList[qsoNumber].exchangeReceived = alertData.exchangeReceived;
+        }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
